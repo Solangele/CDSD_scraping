15660f355ec875f1c7a6fb558a32d9bb5341263d
Solange Devos <angedespretz@gmail.com>
2025-12-09 10:35:36 +0100

    first commit

---
+++ exercices/exo1/exo1.py
@@ -0,0 +1,18 @@
+# Exercice : Récupérer et analyser une page web
+
+# **Site** : http://quotes.toscrape.com (site d'entraînement)
+
+# **Tâches**
+# 1. Récupérer la page d'accueil avec Requests
+import requests
+from urllib.robotparser import RobotFileParser
+
+BASE_URL = "http://quotes.toscrape.com"
+ROBOTS_URL = f"{BASE_URL}/robots.txt"
+
+# 2. Afficher le code de statut
+# 3. Afficher les 500 premiers caractères du HTML
+# 4. Vérifier l'encodage de la page
+# 5. Afficher les headers de la réponse
+# 6. Récupérer le robots.txt du site
+# 7. **Bonus** : Utiliser une session pour faire 3 requêtes successives
